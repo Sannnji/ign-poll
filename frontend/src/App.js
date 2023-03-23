88a4f0c19d76cafcef2e5b7472d3b20c97aec890
James Ji <57771801+Sannnji@users.noreply.github.com>
2023-03-23 11:00:27 -0400

    updated App.js
    added poll component and query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+import Poll from './components/Poll';
 
 function App() {
+  const fetchPolls = () => {
+    return axios.get('http://localhost:8080/poll');
+  };
+
+  // Poll data every 2 seconds
+  const { isLoading, isError, data, error } = useQuery('polls', fetchPolls, {
+    refetchInterval: 2000,
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[65%]">
+        {data.data.map((poll) => (
+          <Poll poll={poll} />
+        ))}
+      </div>
     </div>
   );
 }
